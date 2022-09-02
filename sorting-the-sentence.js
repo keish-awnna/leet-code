3782e684c5d32ba15c66a6ecc3494c6ef07f5aea
Keishawnna Harrell <keishawnnaharrell@keishawnnas-mbp.lan>
2022-09-02 12:13:59 -0700

    sorting the sentence question answered :sparkles:

--- sorting-the-sentence.js
+++ sorting-the-sentence.js
@@ -3,10 +3,7 @@
  * @return {string}
  */
 var sortSentence = function (s) {
-  console.log(s);
   const arr = s.split(" ");
-  console.log(arr);
-  console.log(`-----`);
 
   const sorter = (a, b) => {
     return a[a.length - 1].charCodeAt(0) - b[b.length - 1].charCodeAt(0);
@@ -16,12 +13,8 @@ var sortSentence = function (s) {
   const removingLastChar = arr.map((string) =>
     string.substring(0, string.length - 1)
   );
-  console.log(removingLastChar);
-  console.log(`-----`);
 
   const sortedString = removingLastChar.join(" ");
-  console.log(sortedString);
-  console.log(`*******`);
 
   return sortedString;
 };
