be071b8804baafa1a8b73201edc75d7da1a5123f
Keishawnna Harrell <keishawnnaharrell@keishawnnas-mbp.lan>
2022-10-24 15:55:31 -0700

    added prompt descriptions

--- number-of-good-pairs.js
+++ number-of-good-pairs.js
@@ -1,7 +1,13 @@
 /**
  * @param {number[]} nums
  * @return {number}
- */
+//  1512. Number of Good Pairs
+
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+*/
 var numIdenticalPairs = function (nums) {
   let counter = 0;
   for (let i = 0; i < nums.length; i++) {
