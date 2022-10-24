cfbace299d34efa32ac977a4a1d7385708d76d9b
Keishawnna Harrell <keishawnnaharrell@keishawnnas-mbp.lan>
2022-10-24 15:43:06 -0700

    344. Reverse String completed October 24 2022

---
+++ reverse-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  console.log(s.reverse());
+};
+
+// 344. Reverse string
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
