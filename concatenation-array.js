6aeaf3007b576ab98ed65116bc67665962639cb5
Keishawnna Harrell <keishawnnaharrell@keishawnnas-mbp.lan>
2022-10-31 14:18:53 -0700

    1929. concatenation of array  question answered Oct 31  2022

---
+++ concatenation-array.js
@@ -0,0 +1,10 @@
+// 1929. Concatenation of Array
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  console.log(nums.concat(nums));
+  return nums.concat(nums);
+};
